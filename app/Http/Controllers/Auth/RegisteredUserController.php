97f0dd00058a75983b016207c281818eda07106d
MarkMallari16 <mallariftw01@gmail.com>
2024-05-21 18:39:31 +0800

    email

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -47,6 +47,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect()->route('login');
+        return redirect(RouteServiceProvider::LOGIN);
     }
 }
