f2d2d1ad5e6a1c2e43c97581f607ec78eee5877a
MarkMallari16 <mallariftw01@gmail.com>
2025-03-05 21:24:09 +0800

    remove unnecessary

--- resources/js/Pages/Voter/VoterDashboard.jsx
+++ resources/js/Pages/Voter/VoterDashboard.jsx
@@ -142,12 +142,6 @@ const VoterDashboard = ({ authenticatedName, election, candidatesAll, positionLi
 
     return (
         <div>
-            {/* <div className="bg-white text-gray-900 dark:bg-[#252525] dark:ring-gray-800 dark:text-gray-100 ring-1 ring-gray-300 overflow-hidden shadow-sm rounded-lg mb-8 ">
-                <div className="p-6 ">
-                    <h1 className="text-xl font-medium">Welcome back, {authenticatedName}!</h1>
-                </div>
-            </div> */}
-
             {!isElectionEnded && (
                 <>
                     <PartylistCarousel partylistCarouselData={partyList} />
